bb296717a6689ac836b2886065a8b398e500d1c5
Deepakm52003 <138101919+Deepakm52003@users.noreply.github.com>
2023-06-29 23:40:41 -0700

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,52 @@
+console.log('test')
+var input = document.querySelector('input');
+document.getElementById('one').addEventListener('click',function(){
+    input.value += document.getElementById('one').textContent;
+})
+document.getElementById('two').addEventListener('click',function(){
+    input.value += document.getElementById('two').textContent;
+})
+document.getElementById('three').addEventListener('click',function(){
+    input.value += document.getElementById('three').textContent;
+})
+document.getElementById('four').addEventListener('click',function(){
+    input.value += document.getElementById('four').textContent;
+})
+document.getElementById('five').addEventListener('click',function(){
+    input.value += document.getElementById('five').textContent;
+})
+document.getElementById('six').addEventListener('click',function(){
+    input.value += document.getElementById('six').textContent;
+})
+document.getElementById('seven').addEventListener('click',function(){
+    input.value += document.getElementById('seven').textContent;
+})
+document.getElementById('eight').addEventListener('click',function(){
+    input.value += document.getElementById('eight').textContent;
+})
+document.getElementById('nine').addEventListener('click',function(){
+    input.value += document.getElementById('nine').textContent;
+})
+document.getElementById('zero').addEventListener('click',function(){
+    input.value += document.getElementById('zero').textContent;
+})
+document.getElementById('add').addEventListener('click',function(){
+    input.value += document.getElementById('add').textContent;
+})
+document.getElementById('sub').addEventListener('click',function(){
+    input.value += document.getElementById('sub').textContent;
+})
+document.getElementById('mul').addEventListener('click',function(){
+    input.value += document.getElementById('mul').textContent;
+})
+document.getElementById('div').addEventListener('click',function(){
+    input.value += document.getElementById('div').textContent;
+})
+document.getElementById('equal').addEventListener('click',function(){
+    let ans = input.value;
+    input.value = eval(ans)
+})
+document.getElementById('C').addEventListener('click',function(){
+    input.value = " ";
+})
+
