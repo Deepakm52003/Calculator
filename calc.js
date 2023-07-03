99fc312470a4be6169a57265ff340b50562b4a15
Deepakm52003 <138101919+Deepakm52003@users.noreply.github.com>
2023-07-02 23:49:47 -0700

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,4 +1,3 @@
-console.log('test')
 var input = document.querySelector('input');
 document.getElementById('one').addEventListener('click',function(){
     input.value += document.getElementById('one').textContent;
@@ -30,6 +29,9 @@ document.getElementById('nine').addEventListener('click',function(){
 document.getElementById('zero').addEventListener('click',function(){
     input.value += document.getElementById('zero').textContent;
 })
+document.getElementById('dot').addEventListener('click',function(){
+    input.value += document.getElementById('dot').textContent;
+})
 document.getElementById('add').addEventListener('click',function(){
     input.value += document.getElementById('add').textContent;
 })
@@ -42,11 +44,19 @@ document.getElementById('mul').addEventListener('click',function(){
 document.getElementById('div').addEventListener('click',function(){
     input.value += document.getElementById('div').textContent;
 })
+document.getElementById('mod').addEventListener('click',function(){
+    input.value += document.getElementById('mod').textContent;
+})
 document.getElementById('equal').addEventListener('click',function(){
     let ans = input.value;
     input.value = eval(ans)
 })
 document.getElementById('C').addEventListener('click',function(){
     input.value = " ";
+})
+document.getElementById('del').addEventListener('click',function(){
+    let val = input.value;
+    let del = val.slice(0, val.length - 1);
+    input.value = del;
 })
 
